4628ada9bef73a9bf53bc3d99d2612ec16d42403
tsuiusi <ryantsui786@gmail.com>
2025-04-16 20:20:41 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Experiments/Benchmarks for MLX, Transformers, llama.cpp, llama.cpp via Ollama on Mac
+
+This script was written for MLX and Transformers; llama-bench was used for Llama.cpp, ollama run <model\_name> --verbose was used for ollama. 
+
+> Too lazy to do the whole API integration thing
+
+## Experiment parameters
+- **Device**: Macbook Pro 2023 M2 Pro 16GB RAM 19-Core GPU
+- **Prompt**: "Do you think it's possible to build a time machine?" (38 tokens for Ollama, 13 tokens for MLX)
+- ** 
+
+## Metrics
+- Time to first token
+- Tokens per second 
+- Max memory usage
+
+## Variations
+- Quantized models
+- Batch processing
+- Multi user serving - concurrent requests
+
+## Models tested
+- Llama 3.2 3B
+- Qwen 2.5 14B
+- Gemma 3 4B
+
+## To do
+- More kwargs in argparse
+- Memory profiling for transformers
---
+++ bench.py
@@ -0,0 +1,125 @@
+import time
+import sys
+import argparse
+import tracemalloc
+import torch
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from mlx_lm import load, generate
+
+prompt = "Do you think it's possible to build a time machine?"
+
+def main(args):
+    backend = args.backend
+    model_name = args.model_name
+    quantization = args.quantization
+    max_new_tokens = args.max_new_tokens
+    prompt=args.prompt
+
+    if backend == "mlx":
+        mlx_bench(model_name, quantization, max_new_tokens, prompt)
+    elif backend == "transformers":
+        transformers_bench(model_name, quantization, max_new_tokens, prompt)
+
+def mlx_bench(
+    model_name, 
+    quantization, 
+    max_new_tokens,
+    prompt
+):
+    start = time.time()
+
+    model, tokenizer = load(model_name)
+    loadtime = time.time()
+
+    text = generate(
+        model, 
+        tokenizer, 
+        prompt=prompt, 
+        max_tokens=max_new_tokens,
+        verbose=True
+    )
+    generated = time.time()
+
+    print(f"Loading time: {loadtime - start}")
+    print(f"Generation time: {generated - loadtime}")
+
+
+def transformers_bench(
+    model_name, 
+    quantization, 
+    max_new_tokens,
+    prompt
+):
+    start = time.time()
+
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    load = time.time()
+
+    model_inputs = tokenizer(prompt, return_tensors="pt")
+    tokenized = time.time()
+
+    generated_ids = model.generate(**model_inputs, max_new_tokens=max_new_tokens)
+    tokens = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    generated = time.time()
+
+    print(f"User: {prompt}")
+    print(f"{model_name}: {tokens}")
+
+    print(f"Loading time: {load - start:.4f} seconds")
+    print(f"Tokenizing time: {tokenized - load:.4f} seconds")
+    print(f"Generation time: {generated - tokenized:.4f} seconds")
+    print(f"Number of generated tokens: {len(generated_ids[0])}")
+    print(len(model_inputs["input_ids"][0]))
+    print(f"Tokens per second (tokenization): {len(model_inputs['input_ids'][0]) / (tokenized - load):.2f}")
+    print(f"Tokens per second (eval): {len(generated_ids[0]) / (generated - tokenized):.2f}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Inference backend benchmark script") 
+    parser.add_argument(
+        "--backend",
+        type=str,
+        help=(
+            "The backend you want to test. Valid options are mlx, transformers, llama.cpp, and ollama (Ollama separately from llama.cpp because of scheduling differences)."
+        ),
+        choices=["mlx", "transformers", "llama.cpp", "ollama"],
+        required=True,
+    )
+    parser.add_argument(
+        "--model-name",
+        type=str,
+        help=(
+            "The name of the model you want to test, as provided by HuggingFace"
+        ),
+        required=True,
+    )
+    parser.add_argument(
+        "--quantization",
+        type=str,
+        help=(
+            "Precision at which you want the model to be run. Available precisions are int8, fp4, and nf4"
+        ),
+        required=False,
+        default=None,
+    )
+    parser.add_argument(
+        "--max-new-tokens",
+        type=int,
+        help=(
+            "Maximum number of tokens generated. Defaults to 128."
+        ),
+        default=128
+    )
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        help=(
+            "Prompt used to query the model. Defaults to \"Do you think it's possible to build a time machine?\""
+        ),
+        default="Do you think it's possible to build a time machine?"
+    )
+
+    args = parser.parse_args()
+    main(args)
